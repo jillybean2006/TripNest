884d0117e22b9fc9b07ee51076d3ff100035504c
jillian <kjillian2025@gmail.com>
2026-03-02 16:10:57 -0600

    added models plus created backend db and folders

---
+++ server/models/Trip.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const tripSchema = new mongoose.Schema({
+  destination: {
+    type: String,
+    required: true,
+  },
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.models.Trip || mongoose.model("Trip", tripSchema);
